d12c1c948a8b70bc5e1c64bbd837fd9d328601da
Carlos Arantes <carlosfelipearantes@gmail.com>
2018-04-12 02:12:18 -0300

    Finalizada implementacao de Caso de Uso Rentabilidade, com correcao na rentabilidade.

--- src/controllers/rentabilidade.controller.js
+++ src/controllers/rentabilidade.controller.js
@@ -14,12 +14,22 @@ angular.module('sonhosApp')
             anoFinal = anoAtual + (isNullOrEmpty(vm.periodo) ? 0 : vm.periodo);
 
             var anos = [];
-            var acumulado = [];
+            var acumuladoRF = [];
+            var acumuladoRV = [];
+            var acumuladoMM = [];
+            var acumuladoPrev = [];
             var acumuladoPoup = [];
+            var acumuladoTotal = [];
 
             anos.lenght = vm.periodo;
-            acumulado.lenght = vm.periodo;
+
+            acumuladoRF.lenght = vm.periodo;
+            acumuladoRV.lenght = vm.periodo;
+            acumuladoMM.lenght = vm.periodo;
+            acumuladoPrev.lenght = vm.periodo;
             acumuladoPoup.lenght = vm.periodo;
+
+            acumuladoTotal.lenght = vm.periodo;
 
             var anoContador = anoAtual;
 
@@ -30,42 +40,56 @@ angular.module('sonhosApp')
 
             }
 
-            if( isNullOrEmpty(vm.rf) && isNullOrEmpty(vm.rv)  && isNullOrEmpty(vm.mm) && isNullOrEmpty(vm.prev) ){
+            if(
+                 isNullOrEmpty(vm.rf) && isNullOrEmpty(vm.rv)  && isNullOrEmpty(vm.mm) && isNullOrEmpty(vm.prev) ){
 
                 
 
-                
 
             }else{
 
+                var rf = isNullOrEmpty(vm.rf) ? 0 : (vm.rf);
+                var rv = isNullOrEmpty(vm.rv) ? 0 : (vm.rv);
+                var prev = isNullOrEmpty(vm.prev) ? 0 : (vm.prev);
+                var mm =isNullOrEmpty(vm.mm) ? 0 : (vm.mm);
+                /*    
                 var totalRentabilidade = (isNullOrEmpty(vm.rentabilidadeRf) ? 0 : (vm.rentabilidadeRf/100)*vm.rf)+
                                         (isNullOrEmpty(vm.rentabilidadeRv) ? 0 : (vm.rentabilidadeRv/100)*vm.rv)+
                                         (isNullOrEmpty(vm.rentabilidadePrev) ? 0 : (vm.rentabilidadePrev/100)*vm.prev)+
                                         (isNullOrEmpty(vm.rentabilidadeMm) ? 0 : (vm.rentabilidadeMm/100)*vm.mm);
                                         
+                **/
 
-                var total = ((isNullOrEmpty(vm.rv) ? 0 : vm.rv)+
-                            (isNullOrEmpty(vm.rf) ? 0 : vm.rf)+
-                            (isNullOrEmpty(vm.mm) ? 0 : vm.mm)+
-                            (isNullOrEmpty(vm.prev) ? 0 :vm.prev))*poupancaMensal;
+                var total = rf+rv+prev+mm;
 
-                acumulado[0]=0;
-                acumuladoPoup[0]=0;
+                acumuladoRF[0]=rf;
+                acumuladoRV[0]=rv;
+                acumuladoMM[0]=mm;
+                acumuladoPrev[0]=prev;
+                acumuladoTotal[0]=total;
 
-                for(var i = 1; i< acumuladoPoup.lenght ;i++){
+                acumuladoPoup[0]=total;
 
-                    acumulado[i]=acumulado[i-1]+totalRentabilidade;
-                    acumuladoPoup[i]=acumuladoPoup[i-1]+total;
+                for(var i = 1; i < vm.periodo ;i++){
+
+                    acumuladoRF[i]=acumuladoRF[i-1]*(isNullOrEmpty(vm.rentabilidadeRf) ? 0 : (vm.rentabilidadeRf/100)+1);
+                    acumuladoRV[i]=acumuladoRV[i-1]*(isNullOrEmpty(vm.rentabilidadeRv) ? 0 : (vm.rentabilidadeRv/100)+1);
+                    acumuladoMM[i]=acumuladoMM[i-1]*(isNullOrEmpty(vm.rentabilidadePrev) ? 0 : (vm.rentabilidadePrev/100)+1);
+                    acumuladoPrev[i]=acumuladoPrev[i-1]*(isNullOrEmpty(vm.rentabilidadeMm) ? 0 : (vm.rentabilidadeMm/100)+1);
+                    acumuladoPoup[i]=acumuladoPoup[i-1]*(poupancaMensal+1);
+
+                    acumuladoTotal[i]=acumuladoRF[i]+acumuladoRV[i]+acumuladoPrev[i]+acumuladoMM[i];
+           
 
                 }
 
                 vm.rentabilidade.labels = anos;
                 vm.rentabilidade.series =[['Rentabilidade']];
-                vm.rentabilidade.data = [acumulado];
+                vm.rentabilidade.data = [acumuladoTotal];
 
                 vm.poupanca.labels = anos;
                 vm.poupanca.series = ['Rentabilidade','Poupança']
-                vm.poupanca.data = [ acumulado,acumuladoPoup ];
+                vm.poupanca.data = [ acumuladoTotal,acumuladoPoup ];
 
                 vm.mostrarGraficos = true;
 
